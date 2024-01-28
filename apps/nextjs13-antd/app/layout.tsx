25e419070c8d064ad56eb233e52b4c9c954dd066
Y.Y.F <redbird.fenix@jsmail.fun>
2024-01-28 20:33:40 +0100

    chore: test antd components with subcomponents and without, also add theme

--- apps/nextjs13-antd/app/layout.tsx
+++ apps/nextjs13-antd/app/layout.tsx
@@ -1,4 +1,7 @@
 import { AntdRegistry } from '@ant-design/nextjs-registry';
+import { Inter } from 'next/font/google'
+
+const inter = Inter({ subsets: ['latin'] })
 
 import './global.css';
 
@@ -7,12 +10,16 @@ export const metadata = {
   description: 'Example app with Nextjs 13 and Ant Design',
 };
 
-const RootLayout = ({ children }: React.PropsWithChildren) => (
-  <html lang="en">
-    <body>
-      <AntdRegistry>{children}</AntdRegistry>
-    </body>
-  </html>
-);
-
-export default RootLayout;
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <AntdRegistry>{children}</AntdRegistry>
+      </body>
+    </html>
+  );
+}
