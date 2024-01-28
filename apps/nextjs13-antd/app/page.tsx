6cabfe930ceb36d3fde9a96a7970dd057f2250c8
Y.Y.F. <redbird.fenix@jsmail.fun>
2024-01-28 23:49:33 +0100

    mod: update css, import font, change page

--- apps/nextjs13-antd/app/page.tsx
+++ apps/nextjs13-antd/app/page.tsx
@@ -1,35 +1,26 @@
-import { Button, Space, Divider } from 'antd';
-import Image from 'next/image';
+import { Button, Divider } from 'antd';
+
+import { MainLayout } from '@examples/ui/layouts';
 
 const Home = function Home() {
   return (
-    <>
+    <MainLayout>
       <section
         style={{
           textAlign: 'center',
           marginTop: 48,
           marginBottom: 40,
           padding: 100,
+          background: '#fff',
+          borderRadius: '4px',
         }}
       >
-        <Space align="start">
-          <Image
-            style={{ width: 40, height: 40 }}
-            width={40}
-            height={40}
-            src="https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg"
-            alt="Ant Design"
-          />
-          <h2 style={{ marginBottom: 0 }}>
-            Ant Design (Without Sub Components - there is some problem with it)
-          </h2>
-        </Space>
         <Divider style={{ marginBottom: 60 }}>Divider</Divider>
         <Button type="primary" block href="/with-sub-components">
           With Sub Components
         </Button>
       </section>
-    </>
+    </MainLayout>
   );
 };
 
