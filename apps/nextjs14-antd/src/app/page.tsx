f6bb9f2a10f3397d8edab7bde9194d40fe7401d7
Y.Y.F. <redbird.fenix@jsmail.fun>
2024-01-31 12:19:58 +0100

    mod: AntdRegistry change root layout, frontpage change

--- apps/nextjs14-antd/src/app/page.tsx
+++ apps/nextjs14-antd/src/app/page.tsx
@@ -1,9 +1,32 @@
-import styles from './page.module.css';
+// import styles from './page.module.css';
+'use client';
+import { Layout, Menu } from 'antd';
 
-export default async function Index() {
+const { Header, Content, Footer } = Layout;
+
+export default function Index() {
   return (
-    <div className={styles.container}>
-      <div className={styles.content}>Content</div>
-    </div>
+    <Layout>
+      <Header style={{ display: 'flex', alignItems: 'center' }}>
+        <div className="demo-logo" />
+        <Menu
+          theme="dark"
+          mode="horizontal"
+          defaultSelectedKeys={['1']}
+          items={[
+            { key: '1', label: 'Home' },
+            { key: '2', label: 'Login' },
+            { key: '3', label: 'Register' },
+          ]}
+          style={{ flex: 1, minWidth: 0 }}
+        />
+      </Header>
+      <Content>
+        <div>Content</div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>
+        Y.Y.F. ©{new Date().getFullYear()} Created by Yurii
+      </Footer>
+    </Layout>
   );
 }
