ed7d2d514fdf40e1ff8db71ed811570fc853ff86
Y.Y.F <redbird.fenix@jsmail.fun>
2024-01-31 23:28:17 +0100

    mod: root layout, add login, and register page

--- apps/nextjs14-antd/src/app/page.tsx
+++ apps/nextjs14-antd/src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Row, Col } from 'antd';
+import { Row, Col, Image } from 'antd';
 import { IntroCard, Title, Paragraph } from '@examples/ui/components';
 
 import styles from './page.module.css';
@@ -9,18 +9,27 @@ export default function Index() {
       <Row>
         <Col span={24}></Col>
       </Row>
-      <Row>
+      <Row justify="space-evenly" align="top">
         <Col span={8}>
-          <IntroCard>
-            <Title level={3}>
-              Welcome to Next.js, AntD, Strapi, tRCP example.
-            </Title>
-            <Paragraph>
-              You can login. Or register to this demo.
-            </Paragraph>
-          </IntroCard>
+          <div style={{ height: '100%' }}>
+            <IntroCard>
+              <Title level={3}>
+                Welcome to Next.js, AntD, Strapi, tRCP example.
+              </Title>
+              <Paragraph>You can login. Or register to this demo.</Paragraph>
+            </IntroCard>
+          </div>
         </Col>
-        <Col span={16}></Col>
+        <Col span={14}>
+          <div style={{ height: '100%' }}>
+            <Image
+              className={styles.image}
+              alt="Picture of desk with pc, notebook and office supply"
+              preview={false}
+              src="/retrosupply-jLwVAUtLOAQ-unsplash.jpg"
+            />
+          </div>
+        </Col>
       </Row>
       <Row>
         <Col span={8}></Col>
---
+++ apps/nextjs14-antd/src/app/register/page.tsx
@@ -0,0 +1,12 @@
+import styles from './page.module.css';
+
+/* eslint-disable-next-line */
+export interface RegisterProps {}
+
+export default function Register(props: RegisterProps) {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to Register!</h1>
+    </div>
+  );
+}
