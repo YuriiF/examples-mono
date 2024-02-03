e854bbe1bca03b24fb2bda7687c9a169dae84983
Y.Y.F. <redbird.fenix@jsmail.fun>
2024-02-03 14:02:55 +0100

    feat: dashboard page

---
+++ apps/nextjs14-antd/src/app/dashboard/(overview)/page.tsx
@@ -0,0 +1,53 @@
+import { Suspense } from 'react';
+import Row from 'antd/lib/row';
+import Col from 'antd/lib/col';
+
+import {
+  CardsSkeleton,
+  LatestInvoices,
+  LatestInvoicesSkeleton,
+  RevenueChart,
+  RevenueChartSkeleton,
+} from '@examples/features/dashboard';
+
+import { Cards } from '@examples/features/dashboard/server';
+
+import {
+  ChartCard,
+  G2IntervalChart,
+  G2LineChart,
+  Title,
+} from '@examples/ui/components';
+
+export default async function Page() {
+  return (
+    <>
+      <Title level={2}>Dashboard (Page Title)</Title>
+      <Row gutter={[24, 24]}>
+        <Suspense fallback={<CardsSkeleton />}>
+          <Cards />
+        </Suspense>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <Col span={12}>
+            <ChartCard chart={<G2IntervalChart />} />
+          </Col>
+        </Suspense>
+        <Col span={12}>
+          <Suspense fallback={<LatestInvoicesSkeleton />}>
+            <ChartCard chart={<G2LineChart />} />
+          </Suspense>
+        </Col>
+        <Suspense fallback={<RevenueChartSkeleton />}>
+          <Col span={12}>
+            <RevenueChart />
+          </Col>
+        </Suspense>
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <Col span={12}>
+            <LatestInvoices />
+          </Col>
+        </Suspense>
+      </Row>
+    </>
+  );
+}
