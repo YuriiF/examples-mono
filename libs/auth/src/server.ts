c2757a16129490d0e343d7cf75e791a72c2964c3
Y.Y.F. <redbird.fenix@jsmail.fun>
2024-02-03 18:07:46 +0100

    feat: auth library, actions, login-action, logout, register actions, data, defninitions

---
+++ libs/auth/src/server.ts
@@ -0,0 +1,9 @@
+// Use this file to export React server components
+export * from './lib/hello-server';
+export * from './lib/actions';
+export * from './lib/utils';
+export * from './lib/definitions';
+export * from './lib/login-action';
+export * from './lib/logout-action';
+export * from './lib/register-action';
+
