18c10fc553c903dad9d860d646a6b36fab4bf2f8
Y.Y.F. <redbird.fenix@jsmail.fun>
2024-02-03 18:11:47 +0100

    feat: create dasboard feature, connect to Strapi backend

---
+++ libs/features/dashboard/src/index.ts
@@ -0,0 +1,7 @@
+// Use this file to export React client components (e.g. those with 'use client' directive) or other non-server utilities
+
+export * from './lib/latest-invoices';
+export * from './lib/nav-links';
+export * from './lib/revenue-chart';
+export * from './lib/sidenav';
+export * from './lib/skeletons';
